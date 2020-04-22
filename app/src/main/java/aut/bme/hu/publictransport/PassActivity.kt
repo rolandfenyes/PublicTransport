30aa4365ae84db4852e70ec6889111b755638b6c
rolandfenyes <ftendalan@gmail.com>
2020-04-22 15:15:48 +0200

    companion object added to PassActivity

--- app/src/main/java/aut/bme/hu/publictransport/PassActivity.kt
+++ app/src/main/java/aut/bme/hu/publictransport/PassActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class PassActivity : AppCompatActivity() {
+    companion object {
+        const val KEY_TYPE_STRING = "KEY_TYPE_STRING"
+        const val KEY_DATE_STRING = "KEY_DATE_STRING"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
