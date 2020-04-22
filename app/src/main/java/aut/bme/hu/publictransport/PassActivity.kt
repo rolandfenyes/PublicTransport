34c545d686d793ac5b986bba983cc7c6601690af
rolandfenyes <ftendalan@gmail.com>
2020-04-22 15:13:32 +0200

    PassActivity generated

---
+++ app/src/main/java/aut/bme/hu/publictransport/PassActivity.kt
@@ -0,0 +1,12 @@
+package aut.bme.hu.publictransport
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PassActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pass)
+    }
+}
