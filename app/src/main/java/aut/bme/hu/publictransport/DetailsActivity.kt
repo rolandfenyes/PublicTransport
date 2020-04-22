1cca4940de71d1af3a76c3e6bb21af739cd993a8
rolandfenyes <ftendalan@gmail.com>
2020-04-22 16:12:44 +0200

    price calculator is done

--- app/src/main/java/aut/bme/hu/publictransport/DetailsActivity.kt
+++ app/src/main/java/aut/bme/hu/publictransport/DetailsActivity.kt
@@ -1,16 +1,28 @@
 package aut.bme.hu.publictransport
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CalendarContract
 import android.widget.DatePicker
+import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_details.*
+import java.math.BigDecimal
+import java.time.Duration
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 class DetailsActivity : AppCompatActivity() {
     companion object {
         const val KEY_TRANSPORT_TYPE = "KEY_TRANSPORT_TYPE"
     }
+
+    private val bikeDailyPrice = 700
+    private val busDailyPrice = 1000
+    private val trainDailyPrice = 1500
+    private val boatDailyPrice = 2500
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +51,23 @@ class DetailsActivity : AppCompatActivity() {
     }
 
     private fun getDateString(dp: DatePicker): String {
-        return String.format(Locale.getDefault(), "%04d.%02d.%02d", dp.year, dp.month+1, dp.dayOfMonth)
+        return String.format(Locale.getDefault(), "%04d-%02d-%02d", dp.year, dp.month+1, dp.dayOfMonth)
     }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun calculatePrice(startDate: DatePicker, endDate: DatePicker, transportType: Int): Long {
+        val date1 = LocalDate.parse(getDateString(startDate), DateTimeFormatter.ISO_DATE)
+        val date2 = LocalDate.parse(getDateString(endDate), DateTimeFormatter.ISO_DATE)
+
+        val days = Duration.between(date1, date2).toDays()
+
+        return when(transportType) {
+            ListActivity.TYPE_BUS -> days * busDailyPrice
+            ListActivity.TYPE_BIKE -> days * bikeDailyPrice
+            ListActivity.TYPE_BOAT -> days * boatDailyPrice
+            ListActivity.TYPE_TRAIN -> days * trainDailyPrice
+            else -> 0
+        }
+    }
+
 }
