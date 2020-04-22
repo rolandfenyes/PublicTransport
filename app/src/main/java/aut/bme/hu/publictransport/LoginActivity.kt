ecaba385831e1596b8daf67db5c728baaa2289f6
rolandfenyes <ftendalan@gmail.com>
2020-04-22 12:12:48 +0200

    Theme setting is called before .onCreate

--- app/src/main/java/aut/bme/hu/publictransport/LoginActivity.kt
+++ app/src/main/java/aut/bme/hu/publictransport/LoginActivity.kt
@@ -6,6 +6,13 @@ import android.os.Bundle
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        try {
+            Thread.sleep(1000)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+        setTheme(R.style.AppTheme)
+        
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
