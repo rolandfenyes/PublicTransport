e701cb9abc59f56a37d8400f05e443f4261b15f5
rolandfenyes <ftendalan@gmail.com>
2020-04-22 15:07:53 +0200

    Type passing is done

--- app/src/main/java/aut/bme/hu/publictransport/DetailsActivity.kt
+++ app/src/main/java/aut/bme/hu/publictransport/DetailsActivity.kt
@@ -11,5 +11,17 @@ class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+
+        val transportType = intent.getIntExtra(KEY_TRANSPORT_TYPE, -1);
+
+    }
+
+    private fun getTypeString(transportType: Int): String {
+        return when(transportType) {
+            ListActivity.TYPE_BUS -> "Bus pass"
+            ListActivity.TYPE_BIKE -> "Bike pass"
+            ListActivity.TYPE_TRAIN -> "Train pass"
+            else -> "Unknown pass"
+        }
     }
 }
