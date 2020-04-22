dd43b8ef8876005642c09eb98a6188f87a3005ac
rolandfenyes <ftendalan@gmail.com>
2020-04-22 17:46:52 +0200

    Price calculator fixed, everything is working

--- app/src/main/java/aut/bme/hu/publictransport/DetailsActivity.kt
+++ app/src/main/java/aut/bme/hu/publictransport/DetailsActivity.kt
@@ -4,14 +4,12 @@ import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.CalendarContract
 import android.widget.DatePicker
+import android.widget.RadioButton
 import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_details.*
-import java.math.BigDecimal
-import java.time.Duration
 import java.time.LocalDate
-import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 import java.util.*
 
 class DetailsActivity : AppCompatActivity() {
@@ -24,18 +22,24 @@ class DetailsActivity : AppCompatActivity() {
     private val trainDailyPrice = 1500
     private val boatDailyPrice = 2500
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
 
         val transportType = intent.getIntExtra(KEY_TRANSPORT_TYPE, -1);
         tvTicketType.text = getTypeString(transportType)
+        //tvPrice.text = calculatePrice(transportType).toString()
 
         btnPurchase.setOnClickListener {
             val intent = Intent(this, PassActivity::class.java)
             intent.putExtra(PassActivity.KEY_TYPE_STRING, getTypeString(transportType))
             intent.putExtra(PassActivity.KEY_DATE_STRING, "${getDateString(dpStartDate)} - ${getDateString(dpEndDate)}")
             startActivity(intent)
+        }
+
+        btnCalculate.setOnClickListener {
+            tvPrice.text = calculatePrice(transportType).toString()
         }
 
     }
@@ -51,21 +55,39 @@ class DetailsActivity : AppCompatActivity() {
     }
 
     private fun getDateString(dp: DatePicker): String {
-        return String.format(Locale.getDefault(), "%04d-%02d-%02d", dp.year, dp.month+1, dp.dayOfMonth)
+        return String.format(Locale.getDefault(), "%04d.%02d.%02d", dp.year, dp.month+1, dp.dayOfMonth)
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun calculatePrice(startDate: DatePicker, endDate: DatePicker, transportType: Int): Long {
-        val date1 = LocalDate.parse(getDateString(startDate), DateTimeFormatter.ISO_DATE)
-        val date2 = LocalDate.parse(getDateString(endDate), DateTimeFormatter.ISO_DATE)
+    private fun calculatePrice(transportType: Int): Long {
+        val discountType = rgPriceCategory.checkedRadioButtonId
+        val radioButton: RadioButton
+        radioButton = findViewById(discountType)
 
-        val days = Duration.between(date1, date2).toDays()
+        val discount = getDiscount(radioButton)
+
+
+        val start = LocalDate.of(dpStartDate.year, dpStartDate.month, dpStartDate.dayOfMonth)
+        val end = LocalDate.of(dpEndDate.year, dpEndDate.month, dpEndDate.dayOfMonth)
+
+        var days = ChronoUnit.DAYS.between(start, end)
+        days += 1
 
         return when(transportType) {
-            ListActivity.TYPE_BUS -> days * busDailyPrice
-            ListActivity.TYPE_BIKE -> days * bikeDailyPrice
-            ListActivity.TYPE_BOAT -> days * boatDailyPrice
-            ListActivity.TYPE_TRAIN -> days * trainDailyPrice
+            ListActivity.TYPE_BUS -> days * busDailyPrice * discount / 100
+            ListActivity.TYPE_BIKE -> days * bikeDailyPrice * discount / 100
+            ListActivity.TYPE_BOAT -> days * boatDailyPrice * discount / 100
+            ListActivity.TYPE_TRAIN -> days * trainDailyPrice * discount / 100
+            else -> 0
+        }
+    }
+
+    private fun getDiscount(radioButton: RadioButton): Long {
+        return when(radioButton.text) {
+            "Full price" ->  100
+            "Senior" -> 10
+            "Student" -> 50
+            "Public servant" -> 50
             else -> 0
         }
     }
