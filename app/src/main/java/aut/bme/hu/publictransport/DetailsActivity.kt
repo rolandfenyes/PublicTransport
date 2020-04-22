698bf752c44df79309fbbabda731ced3010dcbce
rolandfenyes <ftendalan@gmail.com>
2020-04-22 14:56:07 +0200

    Types added

--- app/src/main/java/aut/bme/hu/publictransport/DetailsActivity.kt
+++ app/src/main/java/aut/bme/hu/publictransport/DetailsActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class DetailsActivity : AppCompatActivity() {
+    companion object {
+        const val KEY_TRANSPORT_TYPE = "KEY_TRANSPORT_TYPE"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/aut/bme/hu/publictransport/ListActivity.kt
+++ app/src/main/java/aut/bme/hu/publictransport/ListActivity.kt
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class ListActivity : AppCompatActivity() {
+    companion object {
+        const val TYPE_BUS = 1
+        const val TYPE_TRAIN = 2
+        const val TYPE_BIKE = 3
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
